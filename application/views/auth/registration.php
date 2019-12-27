2349ff65264d681499add14b01e3f0ff22fcc17e
dws16 <dauuuussss@gmail.com>
2019-12-27 17:59:55 +0700

    tambahan alert

--- application/views/auth/registration.php
+++ application/views/auth/registration.php
@@ -17,6 +17,7 @@
                   <div class="text-center">
                     <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
                   </div>
+                  <?= $this->session->flashdata('message'); ?>
                   <?= form_open_multipart(base_url('auth/registration')); ?>
                   <div class="form-group">
                     <input type="text" class="form-control form-control-user" id="name" name="name" placeholder="Full Name" value="<?= set_value('name'); ?>">
