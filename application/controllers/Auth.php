697dba90fb46609d192f29d0937128144033cbfa
dws16 <dauuuussss@gmail.com>
2019-12-28 21:24:14 +0700

    typo

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -260,7 +260,7 @@ class Auth extends CI_Controller
                                           <p style="font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; Margin-bottom: 15px;">Halo, ' . $data['name'] . '
                                               <p></p>
                                           </p>
-                                          <p style="font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; Margin-bottom: 15px;">Sebelum kamu dapat menggunakan akun mu pada web b401telematics.com, Konfirmasi emailmu terlebih dahulu untuk mendaftarkan dirimu di Praktikum Rangkaian Digital. </p>
+                                          <p style="font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; Margin-bottom: 15px;">Sebelum kamu dapat menggunakan akunmu pada web praktikum.b401telematics.com, Konfirmasi emailmu terlebih dahulu untuk mendaftarkan dirimu di Praktikum Rangkaian Digital. </p>
                                           <table border="0" cellpadding="0" cellspacing="0" class="btn btn-primary" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%; box-sizing: border-box;">
                                               <tbody>
                                                   <tr>
