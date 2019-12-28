d435b68fb1668b6934f5377f7e9c3b3373848f6b
dws16 <dauuuussss@gmail.com>
2019-12-29 04:39:43 +0700

    my profile

---
+++ application/views/profile/index.php
@@ -0,0 +1,27 @@
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+   <!-- Page Heading -->
+   <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
+
+   <div class="col">
+      <div class="row-lg-6">
+         <?= $this->session->flashdata('message'); ?>
+      </div>
+   </div>
+
+   <div class="card shadow mb-4 border-left-primary">
+      <div class="card-header py-3">
+         <h6 class="m-0 font-weight-bold text-primary">User Profile</h6>
+      </div>
+      <div class="card-body">
+         <div class="row">
+            <div class="col">
+               <p class="card-text">Name :<?= ' ' . $user['name']; ?></p>
+               <p class="card-text">NRP :<?= ' ' . $user['nrp']; ?></p>
+               <p class="card-text">Email :<?= ' ' . $user['email']; ?></p>
+               <p class="card-text"><small class="text-muted">Member since <?= date('d F Y', $user['date_created']); ?></small></p>
+            </div>
+         </div>
+      </div>
+   </div>
---
+++ application/views/templates/sidebar.php
@@ -0,0 +1,60 @@
+<!-- Sidebar -->
+<ul class="navbar-nav sidebar sidebar-dark accordion" style="background-color:#70B9B0;" id="accordionSidebar">
+
+  <!-- Sidebar - Brand -->
+  <a class="sidebar-brand d-flex align-items-center justify-content-center mb-4" href="<?= base_url('profile') ?>">
+    <div class="sidebar-brand-icon rotate-n-15">
+      <i class="fas fa-circle-notch"></i>
+    </div>
+    <div class="sidebar-brand-text mx-3">Portal Praktikum</div>
+  </a>
+
+  <?php
+  $role_id = $this->session->userdata('role_id');
+  $queryMenu = "SELECT `user_menu`.`id`,`menu`
+                    FROM `user_menu` JOIN `user_access_menu`
+                    ON `user_menu`.`id` = `user_access_menu`.`menu_id`
+                    WHERE `user_access_menu`.`role_id` = $role_id
+                    ORDER BY `user_access_menu`.`menu_id` ASC
+                    ";
+  $menu = $this->db->query($queryMenu)->result_array();
+  ?>
+
+  <?php foreach ($menu as $m) : ?>
+
+    <div class="sidebar-heading">
+      <?= $m['menu']; ?>
+    </div>
+
+    <?php
+    $menuId = $m['id'];
+    $querySubMenu = "SELECT *
+                    FROM `user_sub_menu`
+                    WHERE `menu_id` = $menuId
+                    AND `is_active` = 1
+                    ";
+    $SubMenu = $this->db->query($querySubMenu)->result_array();
+    ?>
+
+    <?php foreach ($SubMenu as $sm) : ?>
+      <?php if ($title == $sm['title']) : ?>
+        <li class="nav-item active">
+        <?php else : ?>
+        <li class="nav-item">
+        <?php endif; ?>
+        <a class="nav-link pb-0" href="<?= base_url($sm['url']); ?>">
+          <i class="<?= $sm['icon']; ?>"></i>
+          <span><?= $sm['title']; ?></span></a>
+        </li>
+      <?php endforeach; ?>
+      <!-- Divider -->
+      <hr class="sidebar-divider mt-3">
+    <?php endforeach; ?>
+
+    <!-- Sidebar Toggler (Sidebar) -->
+    <div class="text-center d-none d-md-inline">
+      <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
+
+</ul>
+<!-- End of Sidebar -->
