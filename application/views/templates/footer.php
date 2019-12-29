dc8339276f66e5e9d1be2729db3dbc7c95219d66
dws16 <dauuuussss@gmail.com>
2019-12-30 01:27:21 +0700

    typo

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -18,6 +18,7 @@
 <a class="scroll-to-top rounded" href="#page-top">
   <i class="fas fa-angle-up"></i>
 </a>
+</div>
 
 <!-- Logout Modal -->
 <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
