e6bb3a0935c307acd8fbe61a6e99de3daeb4d924
dws16 <dauuuussss@gmail.com>
2020-01-30 16:32:04 +0700

    ngecilin table

--- application/views/praktikan/kelengkapanpraktikum.php
+++ application/views/praktikan/kelengkapanpraktikum.php
@@ -6,7 +6,7 @@
 
   <!-- DataTales Example -->
   <div class="row justify-content-center mt-5">
-    <div class="col-lg-8">
+    <div class="col-lg-7">
       <div class="card shadow mb-4">
         <div class="card-header py-3">
           <h6 class="m-0 font-weight-bold text-danger">Kelengkapan Praktikum</h6>
