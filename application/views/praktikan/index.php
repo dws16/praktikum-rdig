621e6d01d6c36c15e02458ef38a5c574c248883a
dws16 <dauuuussss@gmail.com>
2020-01-30 18:54:18 +0700

    tes

--- application/views/praktikan/index.php
+++ application/views/praktikan/index.php
@@ -4,7 +4,7 @@
   <!-- Page Heading -->
   <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
 
-  <div class="row">
+  <div class="row justify-content-center">
     <div class="col-xl-3 col-md-6 mb-4">
 
       <!-- P1 Card -->
