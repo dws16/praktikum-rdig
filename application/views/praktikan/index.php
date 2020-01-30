fcf52bdf1366f161a942013ceb6f6f956aadd1c6
dws16 <dauuuussss@gmail.com>
2020-01-30 18:47:29 +0700

    tes

--- application/views/praktikan/index.php
+++ application/views/praktikan/index.php
@@ -8,7 +8,7 @@
     <div class="col-xl-3 col-md-6 mb-4">
 
       <!-- P1 Card -->
-      <div class="card border-left-info shadow h-100 py-2">
+      <div class="card border-left-info shadow py-2">
         <div class="card-body">
           <div class="row no-gutters align-items-center">
             <div class="col mr-2">
@@ -22,7 +22,7 @@
         </div>
       </div>
       <!-- P2 Card -->
-      <div class="card border-left-info shadow h-100 py-2">
+      <div class="card border-left-info shadow py-2">
         <div class="card-body">
           <div class="row no-gutters align-items-center">
             <div class="col mr-2">
