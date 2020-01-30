d1bbb6d8bf919fd79ec907af1d42f51929f2fd2f
dws16 <dauuuussss@gmail.com>
2020-01-30 18:44:44 +0700

    ngerapihin

--- application/views/praktikan/index.php
+++ application/views/praktikan/index.php
@@ -4,137 +4,108 @@
   <!-- Page Heading -->
   <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
 
-  <!-- Content Row -->
   <div class="row">
-    <div class="col">
+    <div class="col-xl-3 col-md-6 mb-4">
+
       <!-- P1 Card -->
-      <div class="row">
-        <div class="col-xl-3 col-md-6 mb-4">
-          <div class="card border-left-info shadow h-100 py-2">
-            <div class="card-body">
-              <div class="row no-gutters align-items-center">
-                <div class="col mr-2">
-                  <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Jadwal</div>
-                  <div class="h5 mb-0 font-weight-bold text-gray-800">Asisten</div>
-                </div>
-                <div class="col-auto">
-                  <div class="h5 mb-0 font-weight-bold text-gray-800">P1</div>
-                </div>
-              </div>
+      <div class="card border-left-info shadow h-100 py-2">
+        <div class="card-body">
+          <div class="row no-gutters align-items-center">
+            <div class="col mr-2">
+              <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Jadwal</div>
+              <div class="h5 mb-0 font-weight-bold text-gray-800">Asisten</div>
+            </div>
+            <div class="col-auto">
+              <div class="h5 mb-0 font-weight-bold text-gray-800">P1</div>
             </div>
           </div>
         </div>
       </div>
-
       <!-- P2 Card -->
-      <div class="row">
-        <div class="col-xl-3 col-md-6 mb-4">
-          <div class="card border-left-info shadow h-100 py-2">
-            <div class="card-body">
-              <div class="row no-gutters align-items-center">
-                <div class="col mr-2">
-                  <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Jadwal</div>
-                  <div class="h5 mb-0 font-weight-bold text-gray-800">Asisten</div>
-                </div>
-                <div class="col-auto">
-                  <div class="h5 mb-0 font-weight-bold text-gray-800">P2</div>
-                </div>
-              </div>
+      <div class="card border-left-info shadow h-100 py-2">
+        <div class="card-body">
+          <div class="row no-gutters align-items-center">
+            <div class="col mr-2">
+              <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Jadwal</div>
+              <div class="h5 mb-0 font-weight-bold text-gray-800">Asisten</div>
+            </div>
+            <div class="col-auto">
+              <div class="h5 mb-0 font-weight-bold text-gray-800">P2</div>
             </div>
           </div>
         </div>
       </div>
-
       <!-- P3 Card -->
-      <div class="row">
-        <div class="col-xl-3 col-md-6 mb-4">
-          <div class="card border-left-info shadow h-100 py-2">
-            <div class="card-body">
-              <div class="row no-gutters align-items-center">
-                <div class="col mr-2">
-                  <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Jadwal</div>
-                  <div class="h5 mb-0 font-weight-bold text-gray-800">Asisten</div>
-                </div>
-                <div class="col-auto">
-                  <div class="h5 mb-0 font-weight-bold text-gray-800">P3</div>
-                </div>
-              </div>
+      <div class="card border-left-info shadow h-100 py-2">
+        <div class="card-body">
+          <div class="row no-gutters align-items-center">
+            <div class="col mr-2">
+              <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Jadwal</div>
+              <div class="h5 mb-0 font-weight-bold text-gray-800">Asisten</div>
+            </div>
+            <div class="col-auto">
+              <div class="h5 mb-0 font-weight-bold text-gray-800">P3</div>
             </div>
           </div>
         </div>
       </div>
-
       <!-- P4 Card -->
-      <div class="row">
-        <div class="col-xl-3 col-md-6 mb-4">
-          <div class="card border-left-info shadow h-100 py-2">
-            <div class="card-body">
-              <div class="row no-gutters align-items-center">
-                <div class="col mr-2">
-                  <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Jadwal</div>
-                  <div class="h5 mb-0 font-weight-bold text-gray-800">Asisten</div>
-                </div>
-                <div class="col-auto">
-                  <div class="h5 mb-0 font-weight-bold text-gray-800">P4</div>
-                </div>
-              </div>
+      <div class="card border-left-info shadow h-100 py-2">
+        <div class="card-body">
+          <div class="row no-gutters align-items-center">
+            <div class="col mr-2">
+              <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Jadwal</div>
+              <div class="h5 mb-0 font-weight-bold text-gray-800">Asisten</div>
+            </div>
+            <div class="col-auto">
+              <div class="h5 mb-0 font-weight-bold text-gray-800">P4</div>
             </div>
           </div>
         </div>
       </div>
-
       <!-- P5 Card -->
-      <div class="row">
-        <div class="col-xl-3 col-md-6 mb-4">
-          <div class="card border-left-info shadow h-100 py-2">
-            <div class="card-body">
-              <div class="row no-gutters align-items-center">
-                <div class="col mr-2">
-                  <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Jadwal</div>
-                  <div class="h5 mb-0 font-weight-bold text-gray-800">Asisten</div>
-                </div>
-                <div class="col-auto">
-                  <div class="h5 mb-0 font-weight-bold text-gray-800">P5</div>
-                </div>
-              </div>
+      <div class="card border-left-info shadow h-100 py-2">
+        <div class="card-body">
+          <div class="row no-gutters align-items-center">
+            <div class="col mr-2">
+              <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Jadwal</div>
+              <div class="h5 mb-0 font-weight-bold text-gray-800">Asisten</div>
+            </div>
+            <div class="col-auto">
+              <div class="h5 mb-0 font-weight-bold text-gray-800">P5</div>
             </div>
           </div>
         </div>
       </div>
-
     </div>
-    <div class="col">
-      <div class="row">
-        <div class="col-lg-7">
-          <div class="card shadow mb-4">
-            <div class="card-header py-3">
-              <h6 class="m-0 font-weight-bold text-danger">Nama Kelompok</h6>
-            </div>
-            <div class="card-body">
-              <div class="table-responsive">
-                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                  <thead>
-                    <tr>
-                      <th>Nama</th>
-                      <th>NRP</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td></td>
-                      <td></td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-            </div>
+
+    <div class="col-xl-3 col-md-6 mb-4">
+      <div class="card shadow mb-4">
+        <div class="card-header py-3">
+          <h6 class="m-0 font-weight-bold text-danger">Nama Kelompok</h6>
+        </div>
+        <div class="card-body">
+          <div class="table-responsive">
+            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+              <thead>
+                <tr>
+                  <th>Nama</th>
+                  <th>NRP</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td></td>
+                  <td></td>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
-
     </div>
+
   </div>
-
 </div>
 
 
