db4c45c1728ab91d5706cabd44f0966389560383
dws16 <dauuuussss@gmail.com>
2020-01-30 15:07:37 +0700

    page praktikan

---
+++ application/controllers/Praktikan.php
@@ -0,0 +1,44 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Praktikan extends CI_Controller
+{
+  public function __construct()
+  {
+    parent::__construct();
+    is_logged_in();
+  }
+
+  public function index()
+  {
+    $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+    $data['title'] = 'Dashboard';
+    $this->load->view('templates/header', $data);
+    $this->load->view('templates/sidebar', $data);
+    $this->load->view('templates/topbar', $data);
+    $this->load->view('praktikan/index', $data);
+    $this->load->view('templates/footer');
+  }
+
+  public function kelengkapanPraktikum()
+  {
+    $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+    $data['title'] = 'Kelengkapan Praktikum';
+    $this->load->view('templates/header', $data);
+    $this->load->view('templates/sidebar', $data);
+    $this->load->view('templates/topbar', $data);
+    $this->load->view('praktikan/kelengkapanpraktikum', $data);
+    $this->load->view('templates/footer');
+  }
+
+  public function buku()
+  {
+    $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+    $data['title'] = 'Kelengkapan Buku';
+    $this->load->view('templates/header', $data);
+    $this->load->view('templates/sidebar', $data);
+    $this->load->view('templates/topbar', $data);
+    $this->load->view('praktikan/buku', $data);
+    $this->load->view('templates/footer');
+  }
+}
---
+++ application/views/praktikan/kelengkapanpraktikum.php
@@ -0,0 +1,39 @@
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+  <!-- Page Heading -->
+  <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
+
+  <!-- DataTales Example -->
+  <div class="card shadow mb-4">
+    <div class="card-header py-3">
+      <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
+    </div>
+    <div class="card-body">
+      <div class="table-responsive">
+        <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+          <thead>
+            <tr>
+              <th>Modul</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tata Tertib</td>
+            </tr>
+            <tr>
+              <td> <a href="#" class="btn btn-danger btn-circle">
+                  <i class="fas fa-fw fa-arrow-down"></i>
+                </a></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- /.container-fluid -->
+
+</div>
+<!-- End of Main Content -->
