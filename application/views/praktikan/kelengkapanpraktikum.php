4096c7caba4c58d3dd5159bd16ae551ac43db51f
dws16 <dauuuussss@gmail.com>
2020-01-30 15:12:14 +0700

    typo

--- application/views/praktikan/kelengkapanpraktikum.php
+++ application/views/praktikan/kelengkapanpraktikum.php
@@ -7,7 +7,7 @@
   <!-- DataTales Example -->
   <div class="card shadow mb-4">
     <div class="card-header py-3">
-      <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
+      <h6 class="m-0 font-weight-bold text-primary">Kelengkapan Praktikum</h6>
     </div>
     <div class="card-body">
       <div class="table-responsive">
@@ -21,7 +21,7 @@
           <tbody>
             <tr>
               <td>Tata Tertib</td>
-              <td> <a href="#" class="btn btn-danger btn-circle">
+              <td> <a href="#" class="btn btn-danger btn-sm btn-circle">
                   <i class="fas fa-fw fa-arrow-down"></i>
                 </a></td>
             </tr>
