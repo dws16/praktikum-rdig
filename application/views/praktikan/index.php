de582f8b1de99bd76edcbfb7ab46e5333896de86
dws16 <dauuuussss@gmail.com>
2020-01-30 18:49:28 +0700

    ngerapihin

--- application/views/praktikan/index.php
+++ application/views/praktikan/index.php
@@ -8,7 +8,7 @@
     <div class="col-xl-3 col-md-6 mb-4">
 
       <!-- P1 Card -->
-      <div class="card border-left-info shadow py-2">
+      <div class="card border-left-info shadow py-2 my-2">
         <div class="card-body">
           <div class="row no-gutters align-items-center">
             <div class="col mr-2">
@@ -22,7 +22,7 @@
         </div>
       </div>
       <!-- P2 Card -->
-      <div class="card border-left-info shadow py-2">
+      <div class="card border-left-info shadow py-2 my-2">
         <div class="card-body">
           <div class="row no-gutters align-items-center">
             <div class="col mr-2">
@@ -36,7 +36,7 @@
         </div>
       </div>
       <!-- P3 Card -->
-      <div class="card border-left-info shadow h-100 py-2">
+      <div class="card border-left-info shadow py-2 my-2">
         <div class="card-body">
           <div class="row no-gutters align-items-center">
             <div class="col mr-2">
@@ -50,7 +50,7 @@
         </div>
       </div>
       <!-- P4 Card -->
-      <div class="card border-left-info shadow h-100 py-2">
+      <div class="card border-left-info shadow py-2 my-2">
         <div class="card-body">
           <div class="row no-gutters align-items-center">
             <div class="col mr-2">
@@ -64,7 +64,7 @@
         </div>
       </div>
       <!-- P5 Card -->
-      <div class="card border-left-info shadow h-100 py-2">
+      <div class="card border-left-info shadow py-2 my-2">
         <div class="card-body">
           <div class="row no-gutters align-items-center">
             <div class="col mr-2">
@@ -79,7 +79,7 @@
       </div>
     </div>
 
-    <div class="col-xl-3 col-md-6 mb-4">
+    <div class="col-xl-5 col-md-7 mb-6">
       <div class="card shadow mb-4">
         <div class="card-header py-3">
           <h6 class="m-0 font-weight-bold text-danger">Nama Kelompok</h6>
