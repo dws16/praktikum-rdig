6535fe2901713e5864b2b762d64443cef08a91ef
dws16 <dauuuussss@gmail.com>
2020-01-30 15:32:25 +0700

    ngecilin table

--- application/views/praktikan/kelengkapanpraktikum.php
+++ application/views/praktikan/kelengkapanpraktikum.php
@@ -5,32 +5,37 @@
   <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
 
   <!-- DataTales Example -->
-  <div class="card shadow mb-4">
-    <div class="card-header py-3">
-      <h6 class="m-0 font-weight-bold text-primary">Kelengkapan Praktikum</h6>
-    </div>
-    <div class="card-body">
-      <div class="table-responsive">
-        <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-          <thead>
-            <tr>
-              <th>Modul</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Tata Tertib</td>
-              <td> <a href="#" class="badge badge-pill badge-danger"><i class="fas fa-fw fa-arrow-down"></i>Download</a>
-            </tr>
-            <tr>
+  <div class="row justify-content-center mt-5">
+    <div class="col-lg-8">
+      <div class="card shadow mb-4">
+        <div class="card-header py-3">
+          <h6 class="m-0 font-weight-bold text-primary">Kelengkapan Praktikum</h6>
+        </div>
+        <div class="card-body">
+          <div class="table-responsive">
+            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+              <thead>
+                <tr>
+                  <th>Modul</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Tata Tertib</td>
+                  <td> <a href="#" class="badge badge-pill badge-danger"><i class="fas fa-fw fa-arrow-down"></i>Download</a>
+                </tr>
+                <tr>
 
-            </tr>
-          </tbody>
-        </table>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     </div>
   </div>
+
 </div>
 <!-- /.container-fluid -->
 
