f9c742a9c53cf6224d18e1b1f07eb9d77dad0dd8
dws16 <dauuuussss@gmail.com>
2020-01-30 14:04:54 +0700

    agak kecilan

--- application/views/profile/index.php
+++ application/views/profile/index.php
@@ -10,17 +10,21 @@
       </div>
    </div>
 
-   <div class="card shadow mb-4 border-left-warning">
-      <div class="card-header py-3">
-         <h6 class="m-0 font-weight-bold text-warning">User Profile</h6>
-      </div>
-      <div class="card-body">
-         <div class="row">
-            <div class="col">
-               <p class="card-text">Name :<?= ' ' . $user['name']; ?></p>
-               <p class="card-text">NRP :<?= ' ' . $user['nrp']; ?></p>
-               <p class="card-text">Email :<?= ' ' . $user['email']; ?></p>
-               <p class="card-text"><small class="text-muted">Member since <?= date('d F Y', $user['date_created']); ?></small></p>
+   <div class="row justify-content-center mt-5">
+      <div class="col-lg-7">
+         <div class="card shadow mb-4 border-left-warning">
+            <div class="card-header py-3">
+               <h6 class="m-0 font-weight-bold text-warning">User Profile</h6>
+            </div>
+            <div class="card-body">
+               <div class="row">
+                  <div class="col">
+                     <p class="card-text">Name :<?= ' ' . $user['name']; ?></p>
+                     <p class="card-text">NRP :<?= ' ' . $user['nrp']; ?></p>
+                     <p class="card-text">Email :<?= ' ' . $user['email']; ?></p>
+                     <p class="card-text"><small class="text-muted">Member since <?= date('d F Y', $user['date_created']); ?></small></p>
+                  </div>
+               </div>
             </div>
          </div>
       </div>
