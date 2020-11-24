3d42429676d5291f77c604499ddf9fd0d366913a
dws16 <dauuuussss@gmail.com>
2020-11-24 20:41:52 +0700

    tombol logout

--- application/views/templates/topbar.php
+++ application/views/templates/topbar.php
@@ -14,21 +14,20 @@
 
       <!-- Topbar Navbar -->
       <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link">
+            <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?= $user['name']; ?></span>
+          </a>
+          <!-- Dropdown - User Information -->
+        </li>
         <div class="topbar-divider d-none d-sm-block"></div>
 
         <!-- Nav Item - User Information -->
         <li class="nav-item dropdown no-arrow">
-          <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?= $user['name']; ?></span>
+          <a class="nav-link text-gray-600" href="#" data-toggle="modal" data-target="#logoutModal">
+            <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2"></i>
+            <span class="small">Logout</span>
           </a>
-          <!-- Dropdown - User Information -->
-          <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
-              <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-              Logout
-            </a>
-          </div>
         </li>
 
       </ul>
