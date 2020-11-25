b02742d753e8a0024520585c3173f0f027346363
dws16 <dauuuussss@gmail.com>
2020-11-25 21:25:56 +0700

    page data praktikan

---
+++ application/views/koordinator/praktikan.php
@@ -0,0 +1,96 @@
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+  <!-- Page Heading -->
+
+  <!-- DataTales Example -->
+  <div class="row justify-content-center mt-5">
+    <div class="col-lg-10">
+      <div class="card shadow mb-4 border-left-danger">
+        <div class="card-header py-3">
+          <h6 class="m-0 font-weight-bold text-danger"><?= $title ?></h6>
+        </div>
+        <div class="card-body">
+          <div class="row">
+            <div class="col mt-1 mb-3">
+              <?= $this->session->flashdata('message'); ?>
+            </div>
+          </div>
+          <div class="table-responsive">
+            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+              <thead>
+                <tr>
+                  <th>Nama Lengkap</th>
+                  <th>NRP</th>
+                  <th>Kelompok</th>
+                  <th>Data</th>
+                </tr>
+              </thead>
+              <tbody>
+                <?php foreach ($list as $l) : ?>
+                  <tr>
+                    <th><?= $l['name']; ?></th>
+                    <th><?= $l['nrp']; ?></th>
+                    <th><?= $l['kelompok']; ?></th>
+                    <th>
+                      <a href="<?= base_url('assets/img/frs/') . $l['frs']; ?>" class="badge badge-pill badge-info"><i class="fas fa-fw fa-file-contract"></i> FRS</a>
+                      <a href="<?= base_url('assets/img/jadwal/') . $l['jadwal']; ?>" class="badge badge-pill badge-info"><i class="fas fa-fw fa-calendar-day"></i> Jadwal</a>
+                    </th>
+                  </tr>
+                <?php endforeach; ?>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</div>
+<!-- /.container-fluid -->
+
+</div>
+<!-- End of Main Content -->
+
+<div class="modal fade" id="kelompokEdit" tabindex="-1" role="dialog" aria-labelledby="kelompokEditLabel" aria-hidden="true">
+  <div class="modal-dialog  modal-lg" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="kelompokEditLabel">Edit Kelompok</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form action="<?= base_url('koordinator/addkelompok') ?>" method="post">
+          <div class="form-group">
+            <label for="kelompok">Nama Kelompok</label>
+            <input class="form-control" id="kelompok" name="kelompok" type="text" required>
+            <div class="invalid-feedback">
+              Nama kelompok sudah dipakai.
+            </div>
+            <div class="valid-feedback">
+              Nama kelompok tersedia
+            </div>
+            <input class="form-control" id="id" name="id" type="text" hidden>
+          </div>
+          <div style="margin-bottom:-4px;">
+            <label for="kelompok">Anggota Kelompok</label>
+          </div>
+          <div id="anggota">
+
+          </div>
+
+          <a href="#" class="btn btn-success btn-icon-split mt-4" id="addAnggota">
+            <span class="icon"><i class="fas fa-user-plus"></i></span>
+            <span class="text">Tambah Anggota</span>
+          </a>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
+        <button type="submit" id="submit" class="btn btn-primary">Edit</button>
+      </div>
+      </form>
+    </div>
+  </div>
+</div>
